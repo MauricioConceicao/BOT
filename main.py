62a9d6c10748bd408f57142ca91462ff5d281cc3
Macaco6 <ninesixblufs@gmail.com>
2023-01-11 20:23:14 -0300

    tentativa 2 de add no git

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+
+
+
+#__vvvvvvvvvvv_configuraçao de uma janela_vvvvvvvv_____
+janela = Tk()
+janela.title("mauricio")
+janela.geometry("400x400")
+texto = Label(janela, text="bem-vindo")
+texto.grid(column=2, row=0, padx=12, pady=12)
+
+
+
+janela.mainloop()
+#____________^^^^^^^^configuraçao de uma janela^^^^^^^^^^^^____________
