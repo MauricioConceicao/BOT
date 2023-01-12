e25bd19bfb6949e3ae3ceb5c62c8408aff48bd82
Macaco6 <ninesixblufs@gmail.com>
2023-01-11 21:16:49 -0300

    add caixa de entrada:
    vnome = Entry(janela)
    vnome.place(x=10, y=365, width=375, height=20)
    
    e caixa de saida:
    
    texto = Label(janela, text="bem-vindo")

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from tkinter import *
 
 
-
+#nome = input("Me diga seu nome:")
 
 
 #__vvvvvvvvvvv_configuraçao de uma janela_vvvvvvvv_____
 janela = Tk()
 janela.title("mauricio")
 janela.geometry("400x400")
+Label(janela, text="nome", background="#dde", foreground="#009", anchor=W).place(x=10, y=10, width=100, height=20)
+#  S = sul , N = norte , E = leste , W = oeste
+#  NE = nordeste , SE = sudeste , SO = sudoeste , NO = noreoeste
 texto = Label(janela, text="bem-vindo")
-texto.grid(column=2, row=0, padx=12, pady=12)
+vnome = Entry(janela)
+vnome.place(x=10, y=365, width=375, height=20)
+#largura = width/x
+#altura = height/y
 
 
 
